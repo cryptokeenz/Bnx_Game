383cf0efaa81173e58cf08fec53052afafc37cf7
cryptokeenz <108882370+cryptokeenz@users.noreply.github.com>
2023-02-28 03:56:18 +0800

    Update bnx_game_v1.0.py

--- bnx_game_v1.0.py
+++ bnx_game_v1.0.py
@@ -15,6 +15,7 @@ from eth_account.messages import encode_defunct
 from loguru import logger
 
 s = 'O&FPGp+on=vX.V@Nc17dsat85TWwgSzfyZLIuq4AYJK6r2kmC3lhbBR9HiEeUDj0MxQ'
+j = 0
 k = 0
 
 def get_uk(account_address):
@@ -68,27 +69,28 @@ def login(private_key):
     :param private_key: 私钥
     :return:
     """
-    for i in range(0,3):  #(0,3) 中的 3 根据自己号数量修改，比如 20 个号就改成 20
-        private_key = [0xe5f8e01ba8e74a5da6ce017ed94d50fbd65d5d1b1937b2818aaf1d881cd5ff82,  #just test,no money
-                0x3fb4c2c2ff545c27300886e7a32d6a8920c38a8f8d21ab8a02361c1ab8834305,
-                0xe224bef742e25a89d8ba21a5686e88fdc8ae99ad74b1d5c44a76d737740144a7]
-        account = Account.from_key(private_key[i])
-        msg = 'You are better than you know!'
-        signature = Account.sign_message(encode_defunct(text=msg), account.key.hex()).signature.hex()
-        uk = get_uk(account.address.lower())
-        key, _tk = get_key()
-        value = get_sign({
-            "account": account.address.lower(), "uk": uk, "ukSign": signature, "shareCode": "", "w": 1, "cv": 212
-        }, _tk, s)
-        params = {
-            'account': account.address.lower(), 'uk': uk, 'ukSign': signature, 'shareCode': '', 'w': 1, 'cv': 212,
-            key: value, '_tk': _tk, '_cv': '212'}
-        response = requests.get(url='https://raid.binaryx.pro/chess/login/doChainLogin', params=params).json()
-        logger.debug(response)
-        if response.get('uid', False):
-            return response['token'], response['uid']
-        else:
-            raise ValueError('登录失败')
+    key = ['0xe5f8e01ba8e74a5da6ce017ed94d50fbd65d5d1b1937b2818aaf1d881cd5ff82',  #just test,no money
+            '0xd8ea7e91b9fc8d998f1dfd8665d655941a8ab8efe84fbde695fff934e4b381cf',
+            '0xa197f86b4efcd834fde56e309827f10c76dc5a4853db3827f195d7bc001a498e',
+            '0xad4d57ae5078218c3a1bd86776ac67675116f5eeb898777fa699d6cdaf85f6f9',
+            '0x20280ffc0767d4be12683f53c7eae687f2dca5d9b566cf40ecc6fd2c65b0cfb5']
+    account = Account.from_key(key[j])
+    msg = 'You are better than you know!'
+    signature = Account.sign_message(encode_defunct(text=msg), account.key.hex()).signature.hex()
+    uk = get_uk(account.address.lower())
+    key, _tk = get_key()
+    value = get_sign({
+        "account": account.address.lower(), "uk": uk, "ukSign": signature, "shareCode": "", "w": 1, "cv": 212
+    }, _tk, s)
+    params = {
+        'account': account.address.lower(), 'uk': uk, 'ukSign': signature, 'shareCode': '', 'w': 1, 'cv': 212,
+        key: value, '_tk': _tk, '_cv': '212'}
+    response = requests.get(url='https://raid.binaryx.pro/chess/login/doChainLogin', params=params).json()
+    logger.debug(response)
+    if response.get('uid', False):
+        return response['token'], response['uid']
+    else:
+        raise ValueError('登录失败')
 
 
 def attack(_token, _uid, _consume=200):
@@ -130,21 +132,23 @@ def enter_demon_king(_token, _uid):
     logger.debug(response)
 
 def bnx_run():
-    global k
+    global k,j
     k = k + 1
-    for i in range(0,3):  #(0,3) 中的 3 根据自己号数量修改，比如 20 个号就改成 20
-        j = str(i + 1)
+    for i in range(0,5):  #(0,10) 中的 10 根据自己需要的循环次数修改
         _account = Account().create()
         logger.debug(_account.address)
         logger.debug(_account.key.hex())
         # TODO 登录
         token, uid = login(_account.key.hex())
+        j = i + 1
         # TODO 打怪
         attack(token, uid)
         # TODO 查询数据
         enter_demon_king(token, uid)
-        print("第"+j+"个号")
-        # time.sleep(random.randint(1,2))  # 随机时间函数，如果出现连接报错可以尝试开启，(1,2)意思为随机延迟 1-2 秒，可自行修改
+        print("第"+str(j)+"个号")
+        time.sleep(random.randint(1,3))  # 随机时间函数，如果出现连接报错可以尝试开启，(1,3)意思为随机延迟 1-3 秒，可自行修改
+    j = 0
+
     print("第" + str(k) + "次循环")
 
 if __name__ == '__main__':
